eb561e8e0497bc1570356aa52c0ef6b974b170ed
Menyalin Alexey <menyalin@icloud.com>
2023-08-30 15:04:39 +0300

    Initial commit

---
+++ apps/s4log/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 's4log',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/s4log',
+};
